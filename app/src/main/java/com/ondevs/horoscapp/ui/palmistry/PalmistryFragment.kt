605782787b5cc9652cc5fb0ec1689d05c42ea172
Rfaze07 <renzofazele@gmail.com>
2025-01-16 17:44:38 -0300

    initial commit

---
+++ app/src/main/java/com/ondevs/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,27 @@
+package com.ondevs.horoscapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ondevs.horoscapp.R
+import com.ondevs.horoscapp.databinding.FragmentLuckBinding
+import com.ondevs.horoscapp.databinding.FragmentPalmistryBinding
+
+
+class PalmistryFragment : Fragment() {
+
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
